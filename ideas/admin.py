f2527f498be987e66306002b3206078d0c516d8c
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-07-08 08:14:29 +0000

    add summernote and download pillow

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Idea
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Idea)
+class IdeaAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+
 
 # Register your models here.
