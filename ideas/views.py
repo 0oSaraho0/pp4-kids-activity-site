d289e158c665dcfa5c8479a00145af15dbea9ab1
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-07-08 08:36:58 +0000

    add IdeaList to ideas/views.py

--- ideas/views.py
+++ ideas/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Idea
+
+
+class IdeaList(generic.ListView):
+    model = Idea
+    queryset = Idea.objects.filter(status=1)order_by('title')
+    template_name = 'ideas.html'
+    paginate_by = 6
+
 
 # Create your views here.
