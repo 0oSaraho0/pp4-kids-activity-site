bed98291ae3d4590377402611cb3f8396c103bfa
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-07-09 09:02:08 +0000

    add search and display options to admin.py

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from .models import Idea
+from .models import Idea, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Idea)
 class IdeaAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'updated_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'updated_on')
     summernote_fields = ('content')
+
+
+@admin.regirster(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('created_on')
+    search_fields = ('name', 'email', 'body')
+
 
 
 
