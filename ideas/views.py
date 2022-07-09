133e1cd049df4dcace3559150c64b1324fcea473
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-07-09 09:58:24 +0000

    correct template name to correct filepath

--- ideas/views.py
+++ ideas/views.py
@@ -5,8 +5,8 @@ from .models import Idea
 
 class IdeaList(generic.ListView):
     model = Idea
-    queryset = Idea.objects.filter(status=1)order_by('title')
-    template_name = 'ideas.html'
+    queryset = Idea.objects.filter(status=1).order_by('title')
+    template_name = 'ideas/ideas.html'
     paginate_by = 6
 
 
