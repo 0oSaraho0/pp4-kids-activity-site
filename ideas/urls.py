da5231bebfe12a71e7369a5ab9e827acce1c6c6d
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-07-10 09:02:42 +0000

    add basic brows ideas page with links

--- ideas/urls.py
+++ ideas/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('ideas/', views.IdeaList.as_view(), name='ideas'),
+    path('<slug:slug>/', views.IdeaDetail.as_view(), name='idea_detail'),
 ]
--- ideas/views.py
+++ ideas/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Idea
 
 
@@ -10,4 +10,22 @@ class IdeaList(generic.ListView):
     paginate_by = 6
 
 
-# Create your views here.
+class IdeaDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Idea.objects.filter(status=1)
+        idea = get_object_or_404(queryset, slug=slug)
+        comments = idea.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if idea.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "ideas/ideas_detail.html",
+            {
+                "idea": idea,
+                "comments": comments,
+                "liked": liked
+            },
+        )
