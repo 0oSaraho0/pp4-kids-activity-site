b1749a0bfff3354c44bfa604ad26886d55eb6bb8
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-07-12 17:09:44 +0000

    Add commenting functionality

--- ideas/views.py
+++ ideas/views.py
@@ -16,7 +16,7 @@ class IdeaDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Idea.objects.filter(status=1)
         idea = get_object_or_404(queryset, slug=slug)
-        comments = idea.comments.filter(approved=True).order_by("-created_on")
+        comments = idea.comments.order_by("-created_on")
         liked = False
         if idea.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -32,3 +32,36 @@ class IdeaDetail(View):
                 
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Idea.objects.filter(status=1)
+        idea = get_object_or_404(queryset, slug=slug)
+        comments = idea.comments.order_by("-created_on")
+        liked = False
+        if idea.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.idea = idea
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "ideas/ideas_detail.html",
+            {
+                "idea": idea,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+                
+            },
+        )
+
