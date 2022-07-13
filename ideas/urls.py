8c8fe0a782afe509e11d8fe1aa1cedf44d55e702
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-07-13 14:13:16 +0000

    connect like url path

--- ideas/urls.py
+++ ideas/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('ideas/', views.IdeaList.as_view(), name='ideas'),
     path('<slug:slug>/', views.IdeaDetail.as_view(), name='idea_detail'),
+    path('like/<slug:slug>', views.IdeaLike.as_view(), name='idea_like'),
 ]
