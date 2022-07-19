38104710e14a3b4b287a0bdb7765a1a0f8b93a55
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-07-19 09:09:02 +0000

    add create idea page and form

--- ideas/forms.py
+++ ideas/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Idea
 from django import forms
 
 
@@ -6,3 +6,23 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class IdeaForm(forms.ModelForm):
+    class Meta:
+        model = Idea
+        fields = (
+            'title',
+            'featured_image',
+            'activity_location',
+            'web_address',
+            'content',
+        )
+
+        lables = {
+            'title': 'Activity Name',
+            'featured_image': 'Picture',
+            'activity_location': 'Location',
+            'web_address': 'Web Link',
+            'content': 'Review',
+        }
