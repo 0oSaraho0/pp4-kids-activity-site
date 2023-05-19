fb6e7fdaca43781e12e507104fe468be5dbb1ee2
0oSaraho0 <0osarahtoddo0@gmail.com>
2023-05-19 14:23:14 +0000

    add deploy to render in settings

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn kidsbored.wsgi
--- kidsbored/settings.py
+++ kidsbored/settings.py
@@ -31,7 +31,11 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['kids-bored.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
